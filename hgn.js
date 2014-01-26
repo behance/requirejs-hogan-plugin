725e03e0744f51d69b46ae5ee44fe76a0b267164
Alex Lee <axal@adobe.com>
2014-01-26 15:11:24 -0500

    Basic functionality for using require to grab partials
    Any partials that have the delimiter character will be considered a
    requirement for the module and pulled in through require()

--- hgn.js
+++ hgn.js
@@ -1,84 +1,166 @@
 /**@license
- * RequireJS Hogan Plugin | v0.3.0 (2013/06/11)
+ * RequireJS Hogan Plugin | v0.4.0
  * Author: Miller Medeiros | MIT License
  */
-define(['hogan', 'text'], function (hogan, text) {
+define(['hogan', 'text', 'module'], function(hogan, text, module) {
+	var DEFAULT_EXTENSION = '.mustache',
+	DEFAULT_DELIMITER = '/',
 
-    var DEFAULT_EXTENSION = '.mustache';
+	pluginConfig = module.config(),
+	_buildMap = {},
+	_buildTemplateText =
+		'define("{{pluginName}}!{{moduleName}}", ["hogan"{{#partials}}, "{{pluginName}}!{{name}}"{{/partials}}], function(hogan){'+
+		'  var tmpl = new hogan.Template({{{fn}}}, "", hogan),'+
+		'      extend = function(a, b) { for (var k in b) { a[k] = b[k]; } return a; },'+
+		'      parts = { {{#partials}}"{{name}}": arguments[{{order}}].template,{{/partials}} };'+
+		'  function render(context, partials, indent) { return tmpl.render(context, extend(parts, partials), indent); }'+
+		'  render.template = tmpl;'+
+		'  return render;'+
+		'});\n',
+	_buildTemplate;
 
-    var _buildMap = {};
-    var _buildTemplateText = 'define("{{pluginName}}!{{moduleName}}", ["hogan"], function(hogan){'+
-                             '  var tmpl = new hogan.Template({{{fn}}}, "", hogan);'+
-                             // need to use apply to bind the proper scope.
-                             '  function render(){ return tmpl.render.apply(tmpl, arguments); } render.template = tmpl; return render;'+
-                             '});\n';
-    var _buildTemplate;
+	function load(name, req, onLoad, config){
+		var hgnConfig = Object.keys(pluginConfig).length ? pluginConfig : (config.hgn || {}),
+		    fileName = name + (hgnConfig && hgnConfig.templateExtension != null ?
+		    				   hgnConfig.templateExtension :
+		    				   DEFAULT_EXTENSION),
+		    compilationOptions = hgnConfig.compilationOptions? mixIn({}, hgnConfig.compilationOptions) : {}
+			delimiter = hgnConfig.delimiter || DEFAULT_DELIMITER;
 
+		// load text files with text plugin
+		text.get(req.toUrl(fileName), function(data) {
+			var compiled = {};
 
-    function load(name, req, onLoad, config){
-        var hgnConfig = config.hgn || {};
-        var fileName = name;
-        fileName += hgnConfig && hgnConfig.templateExtension != null? hgnConfig.templateExtension : DEFAULT_EXTENSION;
+			if (config.isBuild) {
+				// store compiled function if build
+				// and should always be a string
+				compilationOptions.asString = true;
+				compiled.asString = hogan.compile(data, compilationOptions);
+			}
 
-        // load text files with text plugin
-        text.get(req.toUrl(fileName), function(data){
-            var compilationOptions = hgnConfig.compilationOptions? mixIn({}, hgnConfig.compilationOptions) : {};
+			// maybe it's required by some other plugin during build
+			// so return the compiled template even during build
+			var template = hogan.compile(data, compilationOptions),
+			render = bind(template.render, template),
+			partials = template.partials,
+			partialNames = {},
+			reqs = [],
+		   	p;
 
-            if (config.isBuild) {
-                // store compiled function if build
-                // and should always be a string
-                compilationOptions.asString = true;
-                _buildMap[name] = hogan.compile(data, compilationOptions);
-            }
+			// using object map to eliminate duplicates
+			for (p in partials) {
+				partialNames[partials[p].name] = ~partials[p].name.indexOf(delimiter);
+			}
 
-            // maybe it's required by some other plugin during build
-            // so return the compiled template even during build
-            var template = hogan.compile(data, compilationOptions);
-            var render = bind(template.render, template);
-            // add text property for debugging if needed.
-            // it's important to notice that this value won't be available
-            // after build.
-            render.text = template.text;
-            render.template = template;
-            // return just the render method so it's easier to use
-            onLoad( render );
-        });
-    }
+			compiled.partials = partialNames;
+			_buildMap[name] = compiled;
 
-    function bind(fn, context) {
-        return function(){
-            return fn.apply(context, arguments);
-        };
-    }
+			// Object.keys, but filtering by the value
+			for (p in partialNames) {
+				if (partialNames[p]) {
+					reqs.push(p);
+				}
+			}
 
-    function mixIn(target, source) {
-        var key;
-        for (key in source){
-            if ( Object.prototype.hasOwnProperty.call(source, key) ) {
-                target[key] = source[key];
-            }
-        }
-        return target;
-    }
+			// if there are partials in the template, grab them
+			if (reqs.length) {
+				return require(map.call(reqs, function(p) { return module.id+'!'+p; }), function() {
+					var parts = {},
+					wrappedRender = function(context, partials, indent) {
+						return render(context, mixIn(parts, partials), indent);
+					};
 
-    function write(pluginName, moduleName, writeModule){
-        if(moduleName in _buildMap){
-            if (! _buildTemplate) {
-                // using templates to generate compiled templates, so meta :P
-                _buildTemplate = hogan.compile( _buildTemplateText );
-            }
-            var fn = _buildMap[moduleName];
-            writeModule( _buildTemplate.render({
-                pluginName : pluginName,
-                moduleName : moduleName,
-                fn : fn
-            }) );
-        }
-    }
+					for (var i=0; i < reqs.length; ++i) {
+						parts[reqs[i]] = arguments[i].template;
+					}
+					wrappedRender.text = template.text;
+					wrappedRender.template = template;
+					onLoad(wrappedRender);
+				})
+			}
 
-    return {
-        load : load,
-        write : write
-    };
+			// add text property for debugging if needed.
+			// it's important to notice that this value won't be available
+			// after build.
+			render.text = template.text;
+			render.template = template;
+			// return just the render method so it's easier to use
+			onLoad(render);
+		});
+	}
 
+	function bind(fn, context) {
+		return function(){
+			return fn.apply(context, arguments);
+		};
+	}
+
+	function mixIn(target, source) {
+		var key;
+		for (key in source){
+			if (Object.prototype.hasOwnProperty.call(source, key)) {
+				target[key] = source[key];
+			}
+		}
+		return target;
+	}
+
+	// Array.prototype.map() polyfill from
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+	function map(fun /*, thisArg */) {
+		if (this === void 0 || this === null) {
+			throw new TypeError();
+		}
+
+		var t = Object(this);
+		var len = t.length >>> 0;
+		if (typeof fun !== "function") {
+			throw new TypeError();
+		}
+
+		var res = new Array(len);
+		var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+		for (var i = 0; i < len; i++) {
+			// NOTE: Absolute correctness would demand Object.defineProperty
+			//       be used.  But this method is fairly new, and failure is
+			//       possible only if Object.prototype or Array.prototype
+			//       has a property |i| (very unlikely), so use a less-correct
+			//       but more portable alternative.
+			if (i in t) {
+				res[i] = fun.call(thisArg, t[i], i, t);
+			}
+		}
+
+		return res;
+	}
+
+	function write(pluginName, moduleName, writeModule){
+		if (moduleName in _buildMap){
+			if (!_buildTemplate) {
+				// using templates to generate compiled templates, so meta :P
+				_buildTemplate = hogan.compile(_buildTemplateText);
+			}
+			var compiled = _buildMap[moduleName],
+			partials = [],
+			p;
+
+			for (p in compiled.partials) {
+				if (compiled.partials[p]) {
+					partials.push({ name: p, order: partials.length+1 });
+				}
+			}
+
+			writeModule(_buildTemplate.render({
+				pluginName: pluginName,
+				moduleName: moduleName,
+				partials: partials,
+				fn: compiled.asString
+			}));
+		}
+	}
+
+	return {
+		load: load,
+		write: write
+	};
 });
